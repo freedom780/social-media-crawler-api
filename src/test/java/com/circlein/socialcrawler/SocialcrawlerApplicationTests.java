88b62930623dc1562afac77e51967494e785b817
Roman Korobitsin <roman.korobitsin@gmail.com>
2021-07-01 08:43:33 +1000

    intitialised project with Spring Initialzr bootstrap tool

---
+++ src/test/java/com/circlein/socialcrawler/SocialcrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circlein.socialcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialcrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
